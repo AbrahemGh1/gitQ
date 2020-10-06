82fd80962b7a9e30865b10a16772647605649334
AbrahemGh1 <47093661+AbrahemGh1@users.noreply.github.com>
2020-10-06 03:59:17 +0300

    Update Google_map_screen.dart

--- Google_map_screen.dart
+++ Google_map_screen.dart
@@ -1,83 +1,150 @@
+import 'dart:async';
+import 'dart:collection';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_appxy/services/location_services.dart';
-import 'package:flutter_appxy/views/google_map/components/body.dart';
+import 'package:flutter_appxy/size_config.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 
-import '../../constant.dart';
-import '../../size_config.dart';
+import '../../default_button.dart';
 
-class GoogleMapScreen extends StatefulWidget {
-  static var routeName = "views.GoogleMapScreen";
+class Body extends StatefulWidget {
   @override
-  _GoogleMapScreenState createState() => _GoogleMapScreenState();
+  _BodyState createState() => _BodyState();
 }
 
-class _GoogleMapScreenState extends State<GoogleMapScreen> {
+class _BodyState extends State<Body> {
+  MapType mapType = MapType.normal;
+  Completer<GoogleMapController> _controller = Completer();
+  LocationServices _locationServices;
+  LocationData _locationData;
+  CameraPosition _cameraPosition;
+  Set<Marker> allMapMarkers;
+  Marker userPicker;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Provider<LocationServices>(
-      create: (context) => LocationServices(),
-      child: Scaffold(
-        appBar: buildAppBar(),
-        body: Body(),
-      ),
+    _locationServices=Provider.of<LocationServices>(context);
+
+
+
+    return FutureBuilder(
+      future: _locationServices.getLocation().then((LocationData value) => _locationData=value),
+      builder:(context,snapshot){
+        if(_locationData==null){
+         return Center(child: CircularProgressIndicator());
+        }else{
+          _cameraPosition=CameraPosition(target:LatLng(_locationData.latitude,_locationData.longitude),zoom: 19);
+          setupMarkers();
+          return SafeArea(
+            child: Stack(
+              children: [
+                GoogleMap(
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                  initialCameraPosition: _cameraPosition,
+                  mapType: mapType,
+                  compassEnabled: false,
+                  zoomControlsEnabled: false,
+                  markers: allMapMarkers,
+                ),
+
+                Positioned(
+                  left: MediaQuery.of(context).size.width - 70,
+                  top: 50,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * .12,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        RaisedButton(
+                          color: Color.fromRGBO(250, 246, 235, 1),
+                          child: Icon(
+                            Icons.gps_fixed,
+                          ),
+                          onPressed: () async {
+                            final GoogleMapController controller =
+                            await _controller.future;
+                            await _locationServices.getLocation().then((LocationData value) => _locationData=value);
+                            _cameraPosition=CameraPosition(target:LatLng(_locationData.latitude,_locationData.longitude),zoom: 19);
+
+                            controller.animateCamera(CameraUpdate.newCameraPosition(
+                                _cameraPosition));
+                          },
+                        ),
+                        RaisedButton(
+                          color: Color.fromRGBO(250, 246, 235, 1),
+                          onPressed: () {
+                            setState(() {
+                              mapType = mapType == MapType.normal
+                                  ? MapType.satellite
+                                  : MapType.normal;
+                            });
+                          },
+                          child: Icon(MdiIcons.googleEarth, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                    bottom: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        height: 70,
+                        width: MediaQuery.of(context).size.width - 16,
+                        child: RaisedButton(
+                          child: Center(
+                            child: Text(
+                              "تأكيد الموقع",
+                              style: TextStyle(
+                                  fontSize: getProportionateScreenWidth(50),
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          color: const Color(0xFF1DBF73),
+                          onPressed: () {},
+                        ),
+                      ),
+                    )),
+              ],
+            ),
+          );
+        }
+      } ,
     );
   }
 
-  PreferredSize buildAppBar() {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * .13),
-      child: AppBar(
-        backgroundColor: const Color(0XFFF6F6F6),
-        shadowColor: const Color(0XFFB5B5B5),
-        elevation: 1,
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-            size: 30,
-          ),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        title: Text(
-          "حدد موقع",
-          style: headingStyle,
-        ),
-        actions: [],
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(0),
-          child: new Container(
-              margin: const EdgeInsets.only(
-                  left: 20, top: 0, right: 20, bottom: 20),
-              height: MediaQuery.of(context).size.height * 0.05,
-              decoration: new BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                      new BorderRadius.all(new Radius.circular(10.0))),
-              child: new Directionality(
-                  textDirection: TextDirection.rtl,
-                  child: new TextField(
-                    style: new TextStyle(fontSize: 22.0, color: Colors.black),
-                    decoration: new InputDecoration(
-                      filled: true,
-                      fillColor: Color(0XFFE3E3E5),
-                      hintText: 'ابحث عن موقع',
-                      contentPadding: const EdgeInsets.only(
-                          right: 14.0, bottom: 8.0, top: 8.0),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: new BorderSide(color: Colors.transparent),
-                        borderRadius: new BorderRadius.circular(10.0),
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: new BorderSide(color: Colors.transparent),
-                        borderRadius: new BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ))),
-        ),
-      ),
+  onMapCreated(controller) {
+    setState(() {
+      _controller.complete(controller);
+    });
+  }
+
+  void setupMarkers() {
+    allMapMarkers =new HashSet();
+    userPicker=new Marker(
+      markerId: MarkerId("userPicker"),
+      draggable:true,
+      position: LatLng(_locationData.latitude,_locationData.longitude),
+      onTap: (){print(userPicker.toString());}
     );
+    allMapMarkers.add(userPicker);
+    userPicker.toString();
+
+
   }
 }
